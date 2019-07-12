db84d6ccd1256c9fe126ff1c28d40de612a1b7e0
Trevor Fairbank <Trevorfairbank92@gmail.com>
2019-07-12 15:26:04 -0700

    update

--- public/js/movie.js
+++ public/js/movie.js
@@ -10,17 +10,10 @@ $("#searchMovie").on("click", event => {
 
 function getMovies(searchText) {
   console.log("movies");
-  $.get("/api/movie/:" + searchText, function(data) {}).then(function(res) {
-    console.log(res);
-    //console.log(res.results[0].poster_path);
-    // let newMovie = res.results[0].poster_path;
-    // console.log(res.results[0].genre_ids);
-    // console.log(res.results[0].id);
-    // res.results[i].genre_ids
-    let output = "";
+  $.get("/api/movie/:" + searchText, function() {}).then(function(res) {
     for (var i = 0; i < 10; i++) {
-      output +=
-        $(`<div class="col-md-3">
+      // add poster element to page
+      $(`<div class="col-md-3">
                <img  class="moviePoster" data-id="${res.results[i].id}" width="300px" data-stuff="${res.results[i].genre_ids}" src="https://image.tmdb.org/t/p/w500${res.results[i].poster_path}"></img>
                </div >`).appendTo("#movies");
     }
@@ -30,12 +23,10 @@ var genreArr = [];
 const favArr = [];
 $(document).on("click", ".moviePoster", function() {
   genreArr = $(this).attr("data-stuff");
-  
   favArr.push("data-id");
   console.log(genreArr);
   console.log(favArr);
 });
-
 
 // $(".moviePoster").on("click", (e) => {
 //   e.preventDefault();
