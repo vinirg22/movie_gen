f787b28b9d2ed8f92e59a6a9b29a16acdf24465b
Saron <saronnhong@gmail.com>
2019-07-12 18:59:47 -0700

    fix reccommended movies display after survey and display results in one line

--- public/js/movie.js
+++ public/js/movie.js
@@ -1,15 +1,12 @@
-window.onload = function() {
+window.onload = function () {
   let genreNumber = localStorage.getItem("search");
   // alert("search text value is " + searchText);
 
   $.get("/api/movie/" + genreNumber).then(function(res) {
     console.log(res);
-
-    let output = "";
-    for (var i = 0; i < 10; i++) {
-      output += $(`<div class="col-md-3">
-               <img  class="moviePoster" data-id="${res.results[i].id}" width="300px" data-stuff="${res.results[i].genre_ids}" src="https://image.tmdb.org/t/p/w500${res.results[i].poster_path}"></img>
-               </div >`).appendTo("#movies");
+    for (var i = 0; i < 6; i++) {
+      $(`<div><img class="moviePoster" data-id="${res.results[i].id}" width="200px" data-stuff="${res.results[i].genre_ids}" src="https://image.tmdb.org/t/p/w500${res.results[i].poster_path}"></img>
+        </div>`).appendTo("#recMovies");
     }
   });
 };
@@ -26,20 +23,20 @@ $("#searchMovie").on("click", event => {
 
 function getMovies(searchText) {
   console.log("movies");
-  $.get("/api/movie/:" + searchText, function(data) {}).then(function(res) {
+  $.get("/api/movie/:" + searchText, function (data) { }).then(function (res) {
     console.log(res);
-
-    let output = "";
-    for (var i = 0; i < 10; i++) {
-      output += $(`<div class="col-md-3">
-               <img  class="moviePoster" data-id="${res.results[i].id}" width="300px" data-stuff="${res.results[i].genre_ids}" src="https://image.tmdb.org/t/p/w500${res.results[i].poster_path}"></img>
-               </div >`).appendTo("#movies");
-    }
+    $.get("/api/movie/" + genreNumber).then(function(res) {
+      console.log(res);
+      for (var i = 0; i < 6; i++) {
+        $(`<div><img class="moviePoster" data-id="${res.results[i].id}" width="200px" data-stuff="${res.results[i].genre_ids}" src="https://image.tmdb.org/t/p/w500${res.results[i].poster_path}"></img>
+          </div>`).appendTo("#searchMovies");
+      }
+    });
   });
 }
 var genreArr = [];
 const favArr = [];
-$(document).on("click", ".moviePoster", function() {
+$(document).on("click", ".moviePoster", function () {
   genreArr = $(this).attr("data-stuff");
 
   favArr.push("data-id");
--- public/js/survey.js
+++ public/js/survey.js
@@ -1,15 +1,69 @@
-var genreId;
+var genreId = 0;
 function genreNameToId(name) {
- if (name == "family"){
-   genreId = 12;
- }
-    
+  switch (name) {
+    case "action":
+      genreId = 28;
+      break;
+    case "adventure":
+      genreId = 12;
+      break;
+    case "animation":
+      genreId = 16;
+      break;
+    case "comedy":
+      genreId = 35;
+      break;
+    case "crime":
+      genreId = 80;
+      break;
+    case "documentary":
+      genreId = 99;
+      break;
+    case "drama":
+      genreId = 18;
+      break;
+    case "family":
+      genreId = 10751;
+      break;
+    case "fantasy":
+      genreId = 14;
+      break;
+    case "history":
+      genreId = 36;
+      break;
+    case "horror":
+      genreId = 16;
+      break;
+    case "music":
+      genreId = 10402;
+      break;
+    case "mystery":
+      genreId = 9648;
+      break;
+    case "romance":
+      genreId = 10749;
+      break;
+    case "science fiction":
+      genreId = 878;
+      break;
+    case "tv movie":
+      genreId = 10770;
+      break;
+    case "thriller":
+      genreId = 53;
+      break;
+    case "war":
+      genreId = 10752;
+      break;
+    case "western":
+      genreId = 37;
+      break;
+  }
   return genreId;
 }
 
-$("#user-survey").on("submit", function(e) {
+$("#user-survey").on("submit", function (e) {
   e.preventDefault();
-  // alert("I HAVE BEEN CLICKED!!!!!!!FIND ME!!!");
   var surveyResponse = {
     question1: $("#question1").val(),
     question2: $("#question2").val(),
@@ -17,7 +71,7 @@ $("#user-survey").on("submit", function(e) {
     question4: $("#question4").val(),
     question5: $("#question5").val()
   };
-  console.log(surveyResponse);
+  //console.log(surveyResponse);
   $.ajax({
     method: "PUT",
     url: "/api/moviescores",
@@ -25,15 +79,15 @@ $("#user-survey").on("submit", function(e) {
       genres: [question1.value, question2.value, question3.value]
     }
   })
-    .then(function() {
+    .then(function () {
       var searchOne = question1.value;
       alert("value is " + searchOne);
-      let genreAnswer = genreNameToId(searchOne);
-      
+      var genreAnswer = genreNameToId(question1.value) + ", " + genreNameToId(question2.value) + ", " + genreNameToId(question3.value);
+      alert("concat value is " + genreAnswer);
       localStorage.setItem("search", genreAnswer);
       window.location.replace("/movie");
     })
-    .catch(function(err) {
+    .catch(function (err) {
       console.log(err);
       alert(err.responseText);
     });
